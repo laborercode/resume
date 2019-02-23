9bb1f33d708c2acfd6e41b284cc05849af054ef6
Sungbae Yoon <laborercode@naver.com>
2019-02-23 18:22:34 +0900

    initial commit

---
+++ RESUME.md
@@ -0,0 +1,10 @@
+# 윤성배
+
+[![Build Status](https://travis-ci.org/laborercode/resume.svg?branch=master)](https://travis-ci.org/laborercode/resume)
+
+### Features!
+
+### 경력
+
+### 학력
+
---
+++ src/test/java/org/laborercode/resume/AppTest.java
@@ -0,0 +1,12 @@
+package org.laborercode.resume;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+}
